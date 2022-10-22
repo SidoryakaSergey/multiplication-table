18ffe4ec18bc4222e04750e9df6e6dab1bfe0ba9
Thanatus <s.sidoryaka@gmail.com>
2022-10-23 00:40:49 +0300

    add Login test modal window

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('Hell');
+function testStorage() {
+  const player = {
+    name: 'Eva',
+    score: 0,
+  };
+
+  localStorage.setItem(player.name, JSON.stringify(player));
+
+  const dataLocalStorage = localStorage.getItem(player.name);
+  console.log('>>', JSON.parse(dataLocalStorage));
+}
