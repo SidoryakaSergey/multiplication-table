3b7f3fd4b77d70ede2ab5908830fa9fa5b28069f
Thanatus <s.sidoryaka@gmail.com>
2022-10-25 02:07:35 +0300

    add test img

--- js/script.js
+++ js/script.js
@@ -3,9 +3,32 @@ function testStorage() {
     name: 'Eva',
     score: 0,
   };
-
   localStorage.setItem(player.name, JSON.stringify(player));
-
   const dataLocalStorage = localStorage.getItem(player.name);
   console.log('>>', JSON.parse(dataLocalStorage));
 }
+
+const multiplication_table = {
+  1: [[]],
+};
+
+const loginButton = document.getElementById('loginButton');
+const login = document.getElementById('login');
+const loginName = document.getElementById('loginName');
+const namePlayer = document.getElementById('namePlayer');
+
+loginButton.addEventListener('click', () => {
+  const value = loginName.value;
+  if (value.length < 1) {
+    loginName.placeholder = "Треба ввести ім'я";
+  } else {
+    login.style.display = 'none';
+    namePlayer.innerHTML = 'Гравець - ' + value;
+  }
+});
+
+// loginButton.addEventListener('focus', () => {
+//   loginName.style.backgroundColor = '#FFF';
+//   loginName.value = '';
+//   console.log('focus');
+// });
