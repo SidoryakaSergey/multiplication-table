a1d33b6f87b42ddfecdb0e8374b1d73cc94a4fc1
Thanatus <s.sidoryaka@gmail.com>
2022-10-25 20:52:21 +0300

    add Game board

--- js/script.js
+++ js/script.js
@@ -8,22 +8,43 @@ function testStorage() {
   console.log('>>', JSON.parse(dataLocalStorage));
 }
 
-const multiplication_table = {
-  1: [[]],
-};
+const multiplication_table = [];
 
 const loginButton = document.getElementById('loginButton');
 const login = document.getElementById('login');
 const loginName = document.getElementById('loginName');
 const namePlayer = document.getElementById('namePlayer');
+const loginForm = document.getElementById('loginForm');
+const choiceNumbers = document.getElementById('choiceNumbers');
+const buttonStart = document.getElementById('buttonStart');
+const gameBoard = document.getElementById('gameBoard');
+const question = document.getElementById('question');
+const answers = document.getElementById('answers');
 
 loginButton.addEventListener('click', () => {
   const value = loginName.value;
   if (value.length < 1) {
     loginName.placeholder = "Треба ввести ім'я";
   } else {
-    login.style.display = 'none';
+    loginForm.style.display = 'none';
     namePlayer.innerHTML = 'Гравець - ' + value;
+  }
+});
+
+buttonStart.addEventListener('click', () => {
+  choiceNumbers.style.display = 'none';
+  gameBoard.style.display = 'flex';
+  question.innerHTML = '5x6=?';
+});
+
+answers.addEventListener('click', (target) => {
+  console.log(target.path[1]);
+  const el = target.path[1];
+
+  if (el.classList.contains('ansver')) {
+    question.innerHTML = '5x6=30';
+    console.log(el.innerHTML);
+    answers.style.display = 'none';
   }
 });
 
