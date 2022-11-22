b9aea0bfc96d9883144f4dbff6b6652524416ac3
Thanatus <s.sidoryaka@gmail.com>
2022-11-22 03:07:28 +0200

    create createplayerBasketQuestions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 console.log('BEGIN');
 const dataLocalStorage = [];
+const player = {};
+const PREFIX = 'multiplicationTable_';
 
 const choiceNumbers = document.getElementById('choiceNumbers');
 const numbers = document.querySelectorAll('[type="checkbox"]');
@@ -8,8 +10,11 @@ const gameBoard = document.getElementById('gameBoard');
 const question = document.getElementById('question');
 const answers = document.getElementById('answers');
 const cat = document.getElementById('cat');
+const loginFormWrapper = document.getElementById('login__form-wrapper');
 const loginForm = document.getElementById('login__form');
 const listUsers = document.getElementById('list_Users');
+const namePlayer = document.getElementById('name-player');
+const scorePlayer = document.getElementById('score-player');
 
 function createListLoadUserNames() {
   const listUserNames = [];
@@ -30,35 +35,35 @@ const loadUserNames = createListLoadUserNames();
 window.addEventListener('load', () => {
   console.log('LOAD');
 
-  console.log(loadUserNames);
-  if (loadUserNames.length > 0) {
-    createLoginMenu(loadUserNames);
-  } else {
-    listUsers.loginForm.innerHTML += `
-    <p>Enter Name</p>
-    <input type = 'text'>
-      <button type='button'>Ok</button> 
-    `;
-  }
+  createLoginMenu(loadUserNames);
 });
 
 loginForm.addEventListener('click', (target) => {
   const targetID = target.target.id;
+  const targetButton = target.target;
   if (targetID === 'create_player') {
-    console.log('create new');
     const newName = window.prompt('Inpit Name');
     createUserInDataStorage(newName);
     loadUserNames.push(newName);
     createLoginMenu(loadUserNames);
   }
   if (targetID === 'button_ok') {
-    console.log('ok');
+    loginFormWrapper.style.display = 'none';
+    namePlayer.innerHTML = targetButton.dataset.name;
+
+    const objectLocalStor = JSON.parse(
+      localStorage.getItem(PREFIX + targetButton.dataset.name)
+    );
+
+    // for (let el in objectLocalStor) {
+    //   player[el] = objectLocalStor[el];
+    // }
+    Object.assign(player, objectLocalStor);
+    console.log('player ->', player);
   }
   if (targetID === 'button_del') {
-    const buttonDelele = target.target;
-    console.log(buttonDelele.dataset.name);
-    deleteUserInDataStorage(buttonDelele.dataset.name);
-    loadUserNames.splice(loadUserNames.indexOf(buttonDelele.dataset.name), 1);
+    deleteUserInDataStorage(targetButton.dataset.name);
+    loadUserNames.splice(loadUserNames.indexOf(targetButton.dataset.name), 1);
     createLoginMenu(loadUserNames);
   }
 });
@@ -75,25 +80,59 @@ function createLoginMenu(listNames) {
   listUsers.innerHTML = '';
   for (let elem of listNames) {
     listUsers.innerHTML += addPlayerforMenu(elem);
-    console.log(elem);
   }
 }
 
 function createUserInDataStorage(nameUser) {
-  const player = {
-    name: nameUser,
-    score: 0,
-  };
+  player.name = nameUser;
+  player.score = 0;
   player.multiplication_table = multiplication_table;
-  localStorage.setItem(
-    'multiplicationTable_' + player.name,
-    JSON.stringify(player)
-  );
+  localStorage.setItem(PREFIX + player.name, JSON.stringify(player));
 }
 
 function deleteUserInDataStorage(nameUser) {
-  localStorage.removeItem('multiplicationTable_' + nameUser);
+  localStorage.removeItem(PREFIX + nameUser);
 }
+
+const selectedNumbers = [];
+let playerBasketQuestions = [];
+//  >>> Start game\
+buttonStart.addEventListener('click', () => {
+  cat.classList.toggle('cat__wrong');
+  gameBoard.style.display = 'flex';
+  choiceNumbers.style.display = 'none';
+
+  for (let number of numbers) {
+    if (number.checked) {
+      selectedNumbers.push(number.dataset.number);
+    }
+  }
+  playerBasketQuestions = createplayerBasketQuestions();
+
+  console.log('playerBasketQuestions=>', playerBasketQuestions);
+});
+
+function createplayerBasketQuestions() {
+  const { multiplication_table: table } = player;
+  console.log('table=>', table);
+  console.log('selectedNumbers=>', selectedNumbers);
+  const playerTable = table.filter((elem) => {
+    return selectedNumbers.includes(elem.mult1.toString());
+  });
+  // console.log('playerTable=>', playerTable);
+  return playerTable;
+}
+
+answers.addEventListener('click', (target) => {
+  // console.log(target.path[1]);
+  const el = target.path[1];
+
+  if (el.classList.contains('ansver')) {
+    question.innerHTML = '5x6=30';
+    // console.log(el.innerHTML);
+    answers.style.display = 'none';
+  }
+});
 
 // ********** ANALOG CLOCK *******
 let timeNow = new Date();
@@ -116,22 +155,3 @@ const clock_timer = setInterval(() => {
   minuts_arrow.style.transform = `rotateZ(${minuts}deg)`;
   seconds_arrow.style.transform = `rotateZ(${seconds}deg)`;
 }, 1000);
-
-const selectedNumbers = [];
-//  >>> Start game\
-buttonStart.addEventListener('click', () => {
-  cat.classList.toggle('cat__wrong');
-  gameBoard.style.display = 'flex';
-  choiceNumbers.style.display = 'none';
-});
-
-answers.addEventListener('click', (target) => {
-  // console.log(target.path[1]);
-  const el = target.path[1];
-
-  if (el.classList.contains('ansver')) {
-    question.innerHTML = '5x6=30';
-    // console.log(el.innerHTML);
-    answers.style.display = 'none';
-  }
-});
